88d13a1b9209cd0223e691f1dffa08faedbb4795
Jose Marco Nufio Garrido <jose.m.nufio.0@gmail.com>
2024-12-06 11:07:45 +0100

    Commented out renderHTML, needs a rework

--- src/pui-jsx/index.ts
+++ src/pui-jsx/index.ts
@@ -33,7 +33,6 @@ export function genModel(
     ([key, _]) => key != "children",
   );
   if (attrsEntries.length > 0) {
-    template += " ";
     template += renderAttributesTemplate(elem, data, attrsEntries);
   }
   template += ">";
@@ -188,73 +187,75 @@ function renderChildrenTemplate(idxs: Array<number>, elem: PUIElement, data: any
   return template;
 }
 
-export function renderHTML(elem: PUIElement): string {
-  return renderChildHTML("", elem);
-}
 
-function renderChildHTML(id: string, elem: PUIElement): string {
-  const { tag, attrs } = elem;
-  const is_custom = elem.ntype == "custom";
-  let template = `<${tag}`;
-  if (id.length > 0 && is_custom) {
-    template += ` \${ ${id} === }`;
-  }
-  const attrsEntries = Object.entries(attrs).filter(
-    ([key, _]) => key != "children",
-  );
-  // console.log(attrsEntries);
-  if (attrsEntries.length > 0) {
-    template += " ";
-    const eventsData = {} as Record<string, (event: Event) => void>;
-    for (const [key, val] of attrsEntries) {
-      if (key.startsWith("on")) {
-        if (typeof val != "function") {
-          console.error("Expected function for property " + key);
-          continue;
-        }
-        let eventName = key.substring(2);
-        eventName = `${eventName[0].toLowerCase()}${eventName.substring(1)}`;
-        template += ` \${ ${eventName} @=> ${key} }`;
-        eventsData[key] = val as (ev: Event) => void;
-        continue;
-      }
-      if (typeof val == "object") {
-        console.error(
-          "Objects not supported as properties, if you're using states it's not supported yet",
-        );
-        continue;
-      }
-      if (typeof val == "function") {
-        console.warn(
-          `Setting property ${key} as event handler but not using 'on' prefix makes it unclear. It's recommended to use 'on' prefix for events`,
-        );
-        template += ` \${ ${key} @=> ${key} }`;
-        eventsData[key] = val;
-        continue;
-      }
-      template += ` ${key}="${val}"`;
-    }
-  }
-  template += ">";
-  let child_idx = -1;
-  for (const child of elem.children) {
-    child_idx += 1;
-    const childId = is_custom ? `${id}child_${child_idx}__` : id;
-    if (child.ntype == "text") {
-      template += child.tag;
-      continue;
-    }
-    if (child.ntype == "state") {
-      template += `\${ ${childId} }`;
-      continue;
-    }
-    if (child.ntype == "element") {
-      template += renderChildHTML(childId, child as any);
-    }
-  }
-  template += `</${tag}>`;
-  return template;
-}
+// TODO: Handle building a clean HTML page, possibly good idea for SSR
+// export function renderHTML(elem: PUIElement): string {
+//   return genModel(elem).template;
+// }
+
+// function renderChildHTML(id: string, elem: PUIElement): string {
+//   const { tag, attrs } = elem;
+//   const is_custom = elem.ntype == "custom";
+//   let template = `<${tag}`;
+//   if (id.length > 0 && is_custom) {
+//     template += ` \${ ${id} === }`;
+//   }
+//   const attrsEntries = Object.entries(attrs).filter(
+//     ([key, _]) => key != "children",
+//   );
+//   // console.log(attrsEntries);
+//   if (attrsEntries.length > 0) {
+//     template += " ";
+//     const eventsData = {} as Record<string, (event: Event) => void>;
+//     for (const [key, val] of attrsEntries) {
+//       if (key.startsWith("on")) {
+//         if (typeof val != "function") {
+//           console.error("Expected function for property " + key);
+//           continue;
+//         }
+//         let eventName = key.substring(2);
+//         eventName = `${eventName[0].toLowerCase()}${eventName.substring(1)}`;
+//         template += ` \${ ${eventName} @=> ${key} }`;
+//         eventsData[key] = val as (ev: Event) => void;
+//         continue;
+//       }
+//       if (typeof val == "object") {
+//         console.error(
+//           "Objects not supported as properties, if you're using states it's not supported yet",
+//         );
+//         continue;
+//       }
+//       if (typeof val == "function") {
+//         console.warn(
+//           `Setting property ${key} as event handler but not using 'on' prefix makes it unclear. It's recommended to use 'on' prefix for events`,
+//         );
+//         template += ` \${ ${key} @=> ${key} }`;
+//         eventsData[key] = val;
+//         continue;
+//       }
+//       template += ` ${key}="${val}"`;
+//     }
+//   }
+//   template += ">";
+//   let child_idx = -1;
+//   for (const child of elem.children) {
+//     child_idx += 1;
+//     const childId = is_custom ? `${id}child_${child_idx}__` : id;
+//     if (child.ntype == "text") {
+//       template += child.tag;
+//       continue;
+//     }
+//     if (child.ntype == "state") {
+//       template += `\${ ${childId} }`;
+//       continue;
+//     }
+//     if (child.ntype == "element") {
+//       template += renderChildHTML(childId, child as any);
+//     }
+//   }
+//   template += `</${tag}>`;
+//   return template;
+// }
 
 export function renderDOM<T>(
   UI: {
